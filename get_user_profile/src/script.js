24cbfc61fe70c196ee974679d16025885d6ac8e7
Jacob McLachlan <wolvesfanusa@gmail.com>
2023-10-28 15:31:38 -0400

    react app is back

--- get_user_profile/src/script.js
+++ get_user_profile/src/script.js
@@ -11,7 +11,6 @@ if (!code) {
 } else {
   const accessToken = await getAccessToken(clientId, code);
   const profile = await fetchProfile(accessToken);
-  populateUI(profile);
   const result = await fetch(
     "https://api.spotify.com/v1/playlists/37i9dQZF1EQqedj0y9Uwvu/tracks",
     {
@@ -22,25 +21,11 @@ if (!code) {
   console.log(result);
 }
 
-async function fetchProfile(code: string): Promise<UserProfile> {
+async function fetchProfile(code) {
   const result = await fetch("https://api.spotify.com/v1/me", {
     method: "GET",
     headers: { Authorization: `Bearer ${code}` },
   });
 
   return await result.json();
-}
-
-function populateUI(profile: UserProfile) {
-  document.getElementById("displayName")!.innerText = profile.display_name;
-  document.getElementById("avatar")!.setAttribute("src", profile.images[0].url);
-  document.getElementById("id")!.innerText = profile.id;
-  document.getElementById("email")!.innerText = profile.email;
-  document.getElementById("uri")!.innerText = profile.uri;
-  document
-    .getElementById("uri")!
-    .setAttribute("href", profile.external_urls.spotify);
-  document.getElementById("url")!.innerText = profile.href;
-  document.getElementById("url")!.setAttribute("href", profile.href);
-  document.getElementById("imgUrl")!.innerText = profile.images[0].url;
 }
